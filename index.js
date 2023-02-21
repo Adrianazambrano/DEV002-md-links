f2665866a303b756a12f3ba95eae44871c435e2e
Adriana <adrianazambranoc7@gmail.com>
2023-02-21 13:17:59 -0500

    primeras lineas de codigo

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path, options) => {
+  return new Promise((resove, reject) => {
+    //1. indentificamos si la ruta existe
+    if (fs.existsSync(path)) {
+      // convertimos a una ruta absoluta
+    } else {
+      // si no exite rechazamos la promesa
+      reject('La ruta no existe')
+    }
+  })
+}
+
+module.exports = {
+  mdLinks,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,19 @@
-const mdLinks = require('../');
+// const mdLinks = require('../index.js');
+const { mdLinks } = require('../index.js');
 
 
 describe('mdLinks', () => {
+  it ('debe ser una funcion',() => {
+    console.log('FIX ME');
+  });
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  //  it('Deberia devolver una promesa', () => {
+  //   expect(mdLinks).toBe(typeof Promise);
+  // });
+  it('Si el path no existe debe rechazar la promesa', () =>  {
+    return mdLinks('/este/path/no/existe.md').catch((error) => {
+      expect(error).toBe('La ruta no existe');
+    })
   });
 
 });
