e78e5163a874b10f196efc8ed2d41f8b6f6c734c
Adriana <adrianazambranoc7@gmail.com>
2023-02-22 11:32:45 -0500

    dkdkdkd

---
+++ click.js
@@ -0,0 +1,8 @@
+const {mdLinks}= require('./index.js');
+mdLinks('unaruta/que/no/existe.md')
+.then(()=>{
+
+})
+.catch((error) => {
+    console.log(error)
+})
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 
 const mdLinks = (path, options) => {
-  return new Promise((resove, reject) => {
+  return new Promise((resolve, reject) => {
     //1. indentificamos si la ruta existe
     if (fs.existsSync(path)) {
       // convertimos a una ruta absoluta
+      // Probar si la ruta absoluta es un archivo o directorio
+      // si es un archivo devolver un arreglo de este archivo, pero si es un directirioo
+      // sera un arreglo con TODOS los archivos MD, si no hay archivos MD devuelve un array vacio
+      // y si es vacio entonces se rechaza la promesa 
     } else {
       // si no exite rechazamos la promesa
       reject('La ruta no existe')
