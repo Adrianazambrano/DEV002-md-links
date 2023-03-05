f518827597e1cba35c3fd79ae31330b2afb8d530
Adriana <adrianazambranoc7@gmail.com>
2023-03-04 20:21:15 -0500

    terminar funcion para obtener los links de los archivos .md

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 
 const colors = require('colors')
 const fs = require('fs');
-const {existsRoute} = require('./modules')
+const {existsRoute,
+  routeAbsolute,
+  isdirectory,
+  readAllFileDirectory
+} = require('./modules')
   // union de los modulos 
 
 console.log('texto con colores'.bgCyan)
 
 const mdLinks = (pathRouter, options) => {
-  return new Promise((resolve, reject) => {
+     
+    if (existsRoute(pathRouter)) { 
+       
+      const absoluteRoute = routeAbsolute (pathRouter) 
+      if (isdirectory(absoluteRoute)){
+        readAllFileDirectory(absoluteRoute)
+      }
+      else{
+
+      }
+    } 
+    
    
-    if (existsRoute(pathRouter)) {
-      fs.readFile(pathRouter,'utf-8',(error,pathRouter) => {
-        if(error){
-          console.log(error)
-        } else{
-          console.log(pathRouter.bgMagenta)
-          console.log('ext:'+ path.extname(pathRouter).bgBlue)
-        }
-      })
-    } else {
+     else {
       // si no exite rechazamos la promesa
       reject('La ruta no existe')
     }
--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,7 @@
+const { rejects } = require('assert');
 const colors = require('colors')
 const fs = require('fs');
+const { resolve } = require('path');
 const path = require('path')
 
  //1. indentificamos si la ruta existe
@@ -11,15 +13,41 @@ const path = require('path')
    if (isAbsolute === true) {
      return route;
    } else {
-     const convertedRoute = path.resolve(router);
+     const convertedRoute = path.resolve(route);
      return convertedRoute;
    }
  };
  
   //3. Verificamos si es un directorio:
   const isdirectory = (route) => fs.statSync(route).isDirectory() // return true or false
- 
-  // 4. Si es un directorio entramos a leer sus archivos y retorna un array con todos
+
+ // 4. Si es un Archivo,(1)verificamos que la ext sea .md
+ // si es .md  (2) entramos a leer y (3) retornar un array con todos los links
+ const getLinksmd = (route) => {
+  // es .md?:
+    if((fs.path.extname(route) === '.md') ){
+      // extraer los links
+      let arrayLinks = []
+      const links = /(https?:\/\/)?(www\.)?[a-z0-9.-]+\.(a-z)+/gi
+      return new Promise(function(resolve, reject){
+        readFile(pathFile,'utf-8', (error,data) =>{
+            if(error){
+                reject(error)
+            }
+
+           const match =  resolve(data.match(links));
+           arrayLinks.push({match})
+        })
+    }) 
+    }
+    else{
+      // no es un archivo .md/ no hay archivos .md 
+    }
+    //leyendo un archivo:
+  
+ }
+
+  // 5. Si es un directorio entramos a leer sus archivos y retorna un array con todos
 const readAllFileDirectory = (route, arrayOfFiles = []) => {
   const files = fs.readdirSync(route); // leemos cada uno de sus archivos o file
   files.forEach((file) => {
@@ -34,16 +62,8 @@ const readAllFileDirectory = (route, arrayOfFiles = []) => {
   // luego validar sin son .md
 };  
 
+// Verificar si los archivos son md :
 
- // leyendo un archivo:
-  // fs.readFile(pathRouter,'utf-8',(error,pathRouter) => {
-  //   if(error){
-  //     console.log(error)
-  //   } else{
-  //     console.log(pathRouter.bgMagenta)
-  //     console.log('ext:'+ path.extname(pathRouter).bgBlue)
-  //   }
-  // })
  
 
 
